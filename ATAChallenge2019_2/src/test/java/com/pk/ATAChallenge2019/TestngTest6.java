fbafbe3b4c1c6a781a44ec7a19a18892f49e921b
pkuma007 <34452200+pkuma007@users.noreply.github.com>
2019-11-13 15:03:49 +0530

    Add files via upload

--- ATAChallenge2019_2/src/test/java/com/pk/ATAChallenge2019/TestngTest6.java
+++ ATAChallenge2019_2/src/test/java/com/pk/ATAChallenge2019/TestngTest6.java
@@ -4,8 +4,11 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
@@ -20,6 +23,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -50,52 +54,125 @@ public class TestngTest6 {
 	}
 	
 	@Test
-	void nseindiaChromeTest6() throws IOException, InterruptedException {
+	void nseindiaChromeTest6() throws IOException, InterruptedException, ParseException {
 		System.out.println("**************************************************");
 		WebDriverWait wait=new WebDriverWait(driver, 50);
 		//a) Hover on Live Market
-		//main_livemkt
+		Actions menuHover = new Actions(driver);
+		WebElement menuMen = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("main_livemkt")));
+		menuHover.moveToElement(menuMen).build().perform();
 		//b) Click on Top Ten Gainers / Losers
-		//a[contains(text(),'Top Ten Gainers / Losers')]
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(),'Top Ten Gainers / Losers')]"))).click();
 		
-		
+		//Create Excel file
 		FileOutputStream fos = new FileOutputStream("TopGainersLosers.xlsx");                                 
+		XSSFWorkbook workbook = new XSSFWorkbook(); 
 		
-		XSSFWorkbook wkb = new XSSFWorkbook(); 
 		//c) Store the Top gainers table values in an excel sheet
-
-		List<WebElement> irows =   driver.findElements(By.xpath("//*[@id='main']/table[1]/tbody/tr"));     
+		//click Gainers Tab
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='Gainers']"))).click();
+		Thread.sleep(5000);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//table[@id='topGainers']")));
+		
+		List<WebElement> irows = driver.findElements(By.xpath("//table[@id='topGainers']/tbody/tr"));     
+		List<WebElement> icols = driver.findElements(By.xpath("//table[@id='topGainers']/tbody/tr/th"));     
 		int iRowsCount = irows.size();     
-		List<WebElement> icols =   driver.findElements(By.xpath("//*[@id='main']/table[1]/tbody/tr[1]/th"));     
 		int iColsCount = icols.size();     
 		
-		XSSFSheet sheet1 = wkb.createSheet("TopGainers"); 
-		
+		XSSFSheet sheet1 = workbook.createSheet("TopGainers"); 
+		List<String> percentageChangeGainersList = new ArrayList<String>();
 		for (int i=1;i<=iRowsCount;i++)      
 		{      
 			XSSFRow excelRow = sheet1.createRow(i);             
 			
 			for (int j=1; j<=iColsCount;j++)                    
-			{           
-			    WebElement val= driver.findElement(By.xpath("//*[@id='main']/table[1]/tbody/tr["+i+"]/td["+j+"]"));             
-				String a = val.getText();                    
-				System.out.print(a);                            
+			{  
+				WebElement val= null;
+				if(i==1) {
+					val= driver.findElement(By.xpath("//table[@id='topGainers']/tbody/tr["+i+"]/th["+j+"]"));
+				}else {
+					val= driver.findElement(By.xpath("//table[@id='topGainers']/tbody/tr["+i+"]/td["+j+"]"));
+					
+					String val1= driver.findElement(By.xpath("//table[@id='topGainers']/tbody/tr[1]/th["+j+"]")).getText();
+					if(val1.contains("Change")) percentageChangeGainersList.add(val.getText());
+				}
 				
+			    String a = val.getText();                    
+				System.out.println(a);      
+				XSSFCell excelCell = excelRow.createCell(j);                      
+				//excelCell.setCellType(XSSFCell.CELL_TYPE_STRING);                   
+				excelCell.setCellValue(a);   
+			}               
+		}
+		
+		
+		//d) Store all the top losers table values in an excel sheet
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='Losers']"))).click();
+		Thread.sleep(5000);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//table[@id='topLosers']")));
+		irows =   driver.findElements(By.xpath("//table[@id='topLosers']/tbody/tr"));     
+		icols =   driver.findElements(By.xpath("//table[@id='topLosers']/tbody/tr/th"));     
+		iRowsCount = irows.size();     
+		iColsCount = icols.size();     
+		
+		XSSFSheet sheet2 = workbook.createSheet("TopLosers"); 
+		List<String> percentageChangeLosersList = new ArrayList<String>();
+		
+		for (int i=1;i<=iRowsCount;i++)      
+		{      
+			XSSFRow excelRow = sheet2.createRow(i);             
+			
+			for (int j=1; j<=iColsCount;j++)                    
+			{           
+				WebElement val= null;
+				if(i==1) {
+					val= driver.findElement(By.xpath("//table[@id='topLosers']/tbody/tr["+i+"]/th["+j+"]"));
+				}else {
+					val= driver.findElement(By.xpath("//table[@id='topLosers']/tbody/tr["+i+"]/td["+j+"]"));
+					String val1= driver.findElement(By.xpath("//table[@id='topLosers']/tbody/tr[1]/th["+j+"]")).getText();
+					if(val1.contains("Change")) percentageChangeLosersList.add(val.getText());
+				} 
+				String a = val.getText();                    
+				System.out.println(a);      
 				XSSFCell excelCell = excelRow.createCell(j);                      
 				//excelCell.setCellType(XSSFCell.CELL_TYPE_STRING);                   
 				excelCell.setCellValue(a);   
 					  
 			}               
 		}
-		//d) Store all the top losers table values in an excel sheet
+		
 		fos.flush();     
-		wkb.write(fos);     
+		workbook.write(fos);     
 		fos.close();  
 		
 		
 		//e) Assert if the percentage change is high to low for each of Top gainers and losers
-		//f) Extract the date and time when top gainers and top losers data was taken from the NSE website and compare with the system time
+		List<String> unSortedList = percentageChangeGainersList;
+		percentageChangeGainersList.sort(Collections.reverseOrder());
+		Assert.assertEquals(percentageChangeGainersList, unSortedList);
 		
+		unSortedList = percentageChangeLosersList;
+		percentageChangeLosersList.sort(Collections.reverseOrder());
+		Assert.assertEquals(percentageChangeLosersList, unSortedList);
+		//f) Extract the date and time when top gainers and top losers data was taken from the NSE Web site and compare with the system time
+		String dateTime= driver.findElement(By.id("dataTime")).getText();
+		dateTime = dateTime.replaceAll("As on ", "").trim();
+		
+		//String sDate1="As on Nov 11, 2019 16:00:17 IST";  
+		SimpleDateFormat sdf =new SimpleDateFormat("MMM dd, yyyy HH:mm:ss Z");
+		
+		Date NSEdateTime = sdf.parse(dateTime);
+		Date sysDate = sdf.parse(sdf.format(new Date()));
+		
+		if (NSEdateTime.compareTo(sysDate) > 0) {
+            System.out.println("NSEdateTime: "+NSEdateTime+" is after sysDate: "+sysDate);
+        } else if (NSEdateTime.compareTo(sysDate) < 0) {
+            System.out.println("NSEdateTime: "+NSEdateTime+" is before sysDate: "+sysDate);
+        } else if (NSEdateTime.compareTo(sysDate) == 0) {
+            System.out.println("NSEdateTime: "+NSEdateTime+" is equal to sysDate: "+sysDate);
+        } 
+		
+	    System.out.println("**************************************************");
 		
 	}
 	
